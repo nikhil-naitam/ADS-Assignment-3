a0eae322ad6eca3808a06ca3ab9c57849b3dbd03
nikhil-naitam <92074480+nikhil-naitam@users.noreply.github.com>
2023-02-24 00:36:11 +0530

    Add files via upload

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container mt-4 text-center'>
+      <h3>Canada University, Canada</h3>
+      <img src="https://curiocity.com/wp-content/uploads/2021/03/the-university-of-toronto.jpg" className='img img-fluid mt-2' style={{border: '5px solid rgba(0,0,0,.2)', width: '80vw'}} alt="" />
+    </div>
+  )
+}
+
+export default Home
